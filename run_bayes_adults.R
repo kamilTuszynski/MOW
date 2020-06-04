97cdbb93535a96f4cc773a475c52244eb3a6c4b6
Kamil Tuszyński <miltenwml@gmail.com>
2020-06-04 16:05:33 +0200

    added 4 files to run classifciation - this version is ready to be executed git reset .Rhistory!

---
+++ run_bayes_adults.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+library(caret,quietly = TRUE)
+library(e1071,quietly = TRUE)
+
+source("local_classification.R")
+
+data = read.csv("adult_fixed.csv")
+
+set.seed(12345) # for reproducibility
+
+train <- sample(1:nrow(data),size = ceiling(0.999*nrow(data)),replace = FALSE)
+data_train <- data[train,]
+data_test <- data[-train,]
+
+start.time <- Sys.time()
+
+
+predLocal <- matrix(ncol = 2, nrow=0)
+
+for(i in 1:nrow(data_test)) {
+  row <- data_test[i,]
+  local <- localClassification(row, data_train, 8000, algorithm = "NaiveBayes")
+  
+  predLocal = rbind(predLocal, local)
+  
+  cat(sprintf("%s z %s\n", i, nrow(data_test)))
+}
+
+predLocal    <- ifelse ( predLocal[, 2] > 0.5, '>50K', '<=50K' )
+
+
+nb.Model         <- naiveBayes( class~., data = data_train )
+predGlobal    <- predict( nb.Model, newdata = data_test, type = "raw" )
+predGlobal    <- ifelse ( predGlobal[, 2] > 0.5, '>50K', '<=50K' )
+
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+tLocal <- table(data_test$class,predLocal)
+tGlobal <- table(data_test$class,predGlobal)
+confusionMatrix(tLocal)
+confusionMatrix(tGlobal)
---
+++ run_tree_mushrooms.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+
+library(rpart,quietly = TRUE)
+library(caret,quietly = TRUE)
+library(rpart.plot,quietly = TRUE)
+
+source("local_classification.R")
+
+data = read.csv("mushrooms.csv")
+
+set.seed(12345) # for reproducibility
+
+train <- sample(1:nrow(data),size = ceiling(0.999*nrow(data)),replace = FALSE)
+data_train <- data[train,]
+data_test <- data[-train,]
+
+start.time <- Sys.time()
+
+
+predLocal <- factor()
+
+for(i in 1:nrow(data_test)) {
+  row <- data_test[i,]
+  local <- localClassification(row, data_train, 2000, algorithm = "DecisionTree")
+  
+  predLocal = unlist(list(predLocal,local))
+  
+  cat(sprintf("%s z %s\n", i, nrow(data_test)))
+}
+
+tree <- rpart(class~.,
+              data=data_train,
+              method = "class")
+predGlobal <- predict(object=tree,data_test,type="class")
+
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+tLocal <- table(data_test$class,predLocal)
+tGlobal <- table(data_test$class,predGlobal)
+confusionMatrix(tLocal)
+confusionMatrix(tGlobal)
